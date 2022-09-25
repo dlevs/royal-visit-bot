9eba9e30c780ee0424d1394322c14db6300dd039
Daniel Levett <dlevett@hotmail.co.uk>
2022-09-25 21:12:26 +0200

    Remove unnecessary package

--- src/components/GamePiece.tsx
+++ src/components/GamePiece.tsx
@@ -1,5 +1,4 @@
 import { useDraggable } from "@dnd-kit/core";
-import { CSS } from "@dnd-kit/utilities";
 import { css } from "@emotion/react";
 import { Piece } from "../models/Game";
 import { pieceStyles } from "../styles/variables";
@@ -13,6 +12,7 @@ export function GamePiece({ type }: { type: Piece }) {
 	);
 	const group = type === "guard1" || type === "guard2" ? "guard" : type;
 
+	console.log(transform);
 	// TODO: Put flipping back
 	return (
 		<img
@@ -25,13 +25,18 @@ export function GamePiece({ type }: { type: Piece }) {
 				left: 0;
 				right: 0;
 				z-index: ${pieceStyles[group].zIndex};
-				transform: ${
-					CSS.Translate.toString(transform) ?? "translate3d(0, 0, 0)"
-				};
 				cursor: ${active ? "grabbing" : "grab"};
 				touch-action: none;
 				transition: ${active ? "none" : "transform 0.3s ease"};
 			`}
+			// Frequently-changing property outside `css` prop, so a new
+			// className does not need to be computed for every small
+			// mouse movement when drag-n-dropping.
+			style={{
+				transform: transform
+					? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+					: "translate3d(0, 0, 0)",
+			}}
 			alt=""
 		/>
 	);
